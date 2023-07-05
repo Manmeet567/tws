4359df5d912a1b50fdf99bd8f834bc50ccb8a970
Manmeet567 <74705756+Manmeet567@users.noreply.github.com>
2023-07-05 16:36:11 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const UserRoutes = require("./routes/userRoutes");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // middlewares
 app.use(express.json())
@@ -14,8 +15,8 @@ app.use('/api/user', UserRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
  .then(() => {
-     app.listen(process.env.PORT, () => {
-         console.log('Server started at PORT : ' + process.env.PORT)
+     app.listen(PORT, () => {
+         console.log('Server started at PORT : ' + PORT)
      })
  })
  .catch((err) => {
